03bf5a017d20ebf556fdf8026361058b927edb34
Jairo Andres Boada Ayala <jairboay@suramericana.com.co>
2018-09-25 20:59:22 -0500

    corrijo error de care nalga

--- src/main/java/com/udea/cart/controller/ShoppingCartController.java
+++ src/main/java/com/udea/cart/controller/ShoppingCartController.java
@@ -6,14 +6,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.udea.cart.model.Car;
 import com.udea.cart.model.Product;
 
 @RestController
 @RequestMapping(path = "/shoppingcart")
 public class ShoppingCartController {
 
-	@RequestMapping(path = "/deleteProducto", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON)
+	@RequestMapping(path = "/deleteProducto", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public void deleteProduct(@RequestParam String JsonProduct) {
 		try {
 			Product p = new Product();
@@ -25,31 +24,31 @@ public class ShoppingCartController {
 	}
 	
 	/** TO-DO*/
-	@RequestMapping(path = "/addProduct", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON)
+	@RequestMapping(path = "/addProduct", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
 	public void addProduct(@RequestParam String JsonProduct) {
 	
 	}
 	
 	/** TO-DO*/
-	@RequestMapping(path = "/deleteProducto", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON)
+	@RequestMapping(path = "/deleteProducto", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public void cleanCar(@RequestParam String JsonProduct) {
 	
 	}
 	
 	/** TO-DO*/
-	@RequestMapping(path = "/getCar", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON)
+	@RequestMapping(path = "/getCar", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public void getCar(@RequestParam String JsonProduct) {
 	
 	}
 	
 	/** TO-DO*/
-	@RequestMapping(path = "/checkOutcar", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON)
+	@RequestMapping(path = "/checkOutcar", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
 	public void checkOutcar(@RequestParam String JsonProduct) {
 	
 	}
 	
 	/** TO-DO*/
-	@RequestMapping(path = "/CreateCar", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON)
+	@RequestMapping(path = "/CreateCar", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 	public void CreateCar(@RequestParam String persona) {
 	
 	}
