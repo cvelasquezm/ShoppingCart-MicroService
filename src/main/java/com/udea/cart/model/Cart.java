5020d7c94f3e5c8c5c1bb37cec570803859389db
Cesar Andres Velasquez Martinez <cesavemr@suramericana.com.co>
2018-09-25 23:05:04 -0500

    Configurando conexión a DB y colocando anotaciones JPA a entidades

--- src/main/java/com/udea/cart/model/Cart.java
+++ src/main/java/com/udea/cart/model/Cart.java
@@ -2,39 +2,57 @@ package com.udea.cart.model;
 
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
 
 @Entity
+@Table
 public class Cart {
 
+	@Column
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	
+	@GeneratedValue(strategy=GenerationType.AUTO)	
 	private String id;
+	
+	@Column
 	private List<Product> products;
+	
+	@Column
 	private String person;
+	
+	
 	public String getId() {
 		return id;
 	}
+	
+	
 	public void setId(String id) {
 		this.id = id;
 	}
+	
+	
 	public List<Product> getProducts() {
 		return products;
 	}
+	
+	
 	public void setProducts(List<Product> products) {
 		this.products = products;
 	}
+	
+	
 	public String getPerson() {
 		return person;
 	}
+	
+	
 	public void setPerson(String person) {
 		this.person = person;
 	}
-	
-	
 	
 }
--- src/main/java/com/udea/cart/model/Product.java
+++ src/main/java/com/udea/cart/model/Product.java
@@ -1,19 +1,27 @@
 package com.udea.cart.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import com.google.gson.Gson;
 
 @Entity
+@Table
 public class Product {
 	
+	@Column
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private String idProduct;
+	
+	@Column
 	private int count;
+	
+	@Column
 	private double price;
 	
 	
